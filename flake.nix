c9af916b585123beb3caedc1f9fb2a3af7ce0c4d
Jappie3 <jasper22034@gmail.com>
2023-12-18 21:28:15 +0100

    feat(flake): use flake-parts & perSystem

--- flake.nix
+++ flake.nix
@@ -3,15 +3,15 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
   outputs = {
     self,
     nixpkgs,
-  }: let
-    pkgs = import nixpkgs {
-      system = "x86_64-linux";
-    };
+    flake-parts,
+    ...
+  } @ inputs: let
     python-packages = ps:
       with ps; [
         requests # HTTP library
@@ -23,14 +23,24 @@
         types-ipaddress # typing stubs for ipaddress
         #argh # argparse wrapper
       ];
-  in {
-    devShell.x86_64-linux = pkgs.mkShell {
-      buildInputs = with pkgs; [
-        python3
-        black
-        isort
-        (pkgs.python3.withPackages python-packages)
+  in
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "x86_64-linux"
       ];
+      perSystem = {
+        pkgs,
+        system,
+        ...
+      }: {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            python3
+            black
+            isort
+            (pkgs.python3.withPackages python-packages)
+          ];
+        };
+      };
     };
-  };
 }
